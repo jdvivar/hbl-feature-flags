6fb6f8b30c784c5cc8db47d9fdec6f438be74876
Daniel Vivar <jdvivar@gmail.com>
2019-02-14 20:04:18 +0000

    Adding feature flags api

---
+++ client/src/services/FeatureFlagsApi.js
@@ -0,0 +1,7 @@
+const apiRoot = 'http://localhost:3000/flags'
+
+const FeatureFlagsApi = {
+  get: async () => (await fetch(apiRoot, { method: "GET" })).json()
+}
+
+export default FeatureFlagsApi
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -2,17 +2,35 @@
   <div>
     <h1>home view</h1>
     <HelloWorld></HelloWorld>
+
+    <div class="ff-list" v-for="flag in flags" :key="flag.id">
+      <div class="ff-item">
+        {{ flag }}
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import FeatureFlagsApi from '@/services/FeatureFlagsApi'
 
 export default {
   name: 'home',
+  data: function() {
+    return {
+      flags: {}
+    }
+  },
   components: {
     HelloWorld
+  },
+  beforeRouteEnter(to, from, next) {
+    next(vm => {
+      FeatureFlagsApi.get().then(answer => {
+        vm.flags = answer
+      })
+    })
   }
 }
 </script>
