c655c3e833dd1df2b88043418a40b6454e2e4e82
Daniel Vivar <jdvivar@gmail.com>
2019-02-21 19:03:43 +0000

    Move google sign in button into its own component

---
+++ client/src/components/GoogleSignInButton.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <button type="button" class="nes-btn" @click="onClickSignIn">
+      <i class="nes-icon google"></i>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GoogleSignInButton',
+  methods: {
+    onClickSignIn() {
+      this.$router.push('flags')
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+button {
+  padding: 20px;
+}
+
+</style>
--- client/src/views/Auth.vue
+++ client/src/views/Auth.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="Auth">
     <h3>Press button to start</h3>
-    <router-link to="flags">
-      <button type="button" class="nes-btn">
-        <i class="nes-icon google"></i>
-      </button>
-    </router-link>
+    <GoogleSignInButton></GoogleSignInButton>
   </div>
 </template>
 
 <script>
+import GoogleSignInButton from '@/components/GoogleSignInButton'
 
 export default {
-  name: 'Auth'
+  name: 'Auth',
+  components: {
+    GoogleSignInButton
+  }
 }
 </script>
 
@@ -31,10 +31,6 @@ export default {
 h3 {
   animation: blink 1s linear infinite;
   margin-bottom: 60px;
-}
-
-button {
-  padding: 20px;
 }
 
 </style>
